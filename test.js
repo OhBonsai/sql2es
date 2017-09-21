58e0e7b55868154b1fa06a22177d7cb4b60b0d52
OhBonsai <letbonsaibe@gmail.com>
2017-09-21 11:06:47 +0800

    update test case

--- test.js
+++ test.js
@@ -3,15 +3,22 @@ let parser = require('./index.js');
 
 const cases = {
     case1: `a=b`,
-    result1: {term: {a:"b"}}
+    result1: {term: {a:"b"}},
+    case2: `(a=b)`,
+    result2: {term: {a:"b"}},
 };
-
 
 describe('Test Function', function() {
 
-    describe('Simple YMember test', function() {
+    describe('Simple YMember Test', function() {
         it(``, function() {
             assert.equal(JSON.stringify(cases.result1), JSON.stringify(parser.parse(cases.case1)));
+        });
+    });
+
+    describe('Simple YMember With Bracket Test', function() {
+        it(``, function() {
+            assert.equal(JSON.stringify(cases.result2), JSON.stringify(parser.parse(cases.case2)));
         });
     });
 });
